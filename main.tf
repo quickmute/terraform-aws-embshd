29676b99aea3fa91e8f05345bf12ae22b643eb82
quickmute <hyon24@gmail.com>
2020-07-28 11:06:57 -0500

    Update main.tf
    Timestamp_output

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 variable "timestamps" {
   type    = bool
-  default = false
+  default = true
 }
 locals {
   timestamps_tags = var.timestamps == true ? {
     "CreationDate"    = formatdate("YYYY-MM-DD hh:mm:ssZZZZZ", timestamp())
     "ModifiedDate"    = formatdate("YYYY-MM-DD hh:mm:ssZZZZZ", timestamp())
     } : {}
+}
+
+output "timestamp"{
+  value = local.timestamps_tags  
 }
